cf29bf54690259955f52d52bc049cfa62d3ef8c7
Sebastiano Poggi <poggos@gmail.com>
2020-06-16 19:43:34 +0200

    Migrate :app Gradle project to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+android {
+    compileSdkVersion(29)
+    buildToolsVersion("29.0.3")
+
+    defaultConfig {
+        applicationId = "me.seebrock3r.elevationtester"
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 6
+        versionName = "3.2.1"
+    }
+
+    packagingOptions {
+        exclude("META-INF/atomicfu.kotlin_module")
+    }
+}
+
+dependencies {
+    implementation("androidx.annotation:annotation:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.core:core-ktx:1.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta7")
+    implementation("com.google.android.material:material:1.1.0")
+}
